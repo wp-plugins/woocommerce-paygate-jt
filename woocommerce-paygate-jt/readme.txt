4b11ef193160e2a193e6b2adca3304674b82284b
ohead <ohead@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-10 05:03:41 +0000

    조건부 무료 배송 클레스 추가
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-paygate-jt/trunk@785680 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ woocommerce-paygate-jt/readme.txt
@@ -0,0 +1,27 @@
+=== bbPress ===
+Contributors: Jeong daeho
+Tags: woocommerce, korea, paygate, paymentgateway
+Requires at least: 3.5
+Tested up to: 3.6
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+
+== Description ==
+
+woocommerce 플러그인의 한국 결제(paygate)를 추가합니다.
+
+플러그인 사용을 위해서는 paygate측과 별도 계약이 성립되어야 하며, 관련 내용은 플러그인 설정 페이지에서 추가 되어야 적용됩니다.
+
+API 인증값은 paygate측의 암호화 관련 부분을 사용하게 하며 보안을 위해 꼭 사용하실 것을 권장합니다.
+
+API 인증값을 사용하지 않을 경우 최소한의 보안 처리만 가능하며 보안상 취약점이 생길 수 있습니다. 
+
+
+
+
+
+== Changelog ==
+
+= 0.0.3 =
+* 배송 클레스 추가
--- woocommerce-paygate-jt/woocommerce-koreapack.php
+++ woocommerce-paygate-jt/woocommerce-koreapack.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce Paygate JT
  * Plugin URI: http://www.studio-jt.co.kr
  * Description: woocommerce paygate 결제모듈
- * Version: 0.2.0
+ * Version: 0.3.0
  * Author: 스튜디오 제이티 (support@studio-jt.co.kr)
  * Author URI: studio-jt.co.kr
  *
@@ -19,6 +19,7 @@ class WC_Korea_Pack {
 	private static $instance;
 	//public $gateway_items = array( 'openxpay', 'paygate' );
 	public $gateway_items = array( 'paygate' );
+	public $shipping_items = array( 'condition-on-free' );
         
 	private function __construct() { /* Do nothing here */ }
         
@@ -48,11 +49,15 @@ class WC_Korea_Pack {
 		$this->gateway_dir 		= apply_filters( 'wc_korea_pack_gateway_dir', trailingslashit( $this->plugin_dir . 'gateway'  ) );
 		$this->gateway_url 		= apply_filters( 'wc_korea_pack_gateway_url', trailingslashit( $this->plugin_url . 'gateway'  ) );
 		
-		// Languages
-		$this->lang_dir     	= apply_filters( 'wc_korea_pack_lang_dir',     trailingslashit( $this->plugin_dir . 'languages' ) );
-		
 		//Gateway list item
 		$this->gateway_items	= apply_filters( 'wc_korea_pack_gateway', $this->gateway_items );
+		
+		//shipping
+		$this->shipping_dir 	= apply_filters( 'wc_korea_pack_shipping_dir', trailingslashit( $this->plugin_dir . 'shipping'  ) );
+		$this->shipping_url 	= apply_filters( 'wc_korea_pack_shipping_url', trailingslashit( $this->plugin_url . 'shipping'  ) );
+		
+		// Languages
+		$this->lang_dir     	= apply_filters( 'wc_korea_pack_lang_dir',     trailingslashit( $this->plugin_dir . 'languages' ) );
 		
 	}
 	
@@ -60,8 +65,14 @@ class WC_Korea_Pack {
 		
 		require_once( $this->includes_dir . 'functions.php' );
 		
+		//gateway load
 		foreach( $this->gateway_items as $gateway_item ) {
 			require_once( $this->gateway_dir . $gateway_item .'/'. $gateway_item .'.php' );	
+		}
+
+		//shipping load
+		foreach( $this->shipping_items as $shipping_item ){
+			require_once( $this->shipping_dir . $shipping_item .'.php' );
 		}
 	}
 	
